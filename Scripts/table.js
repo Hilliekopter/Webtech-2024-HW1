80bfb5d3eedda1d51e12916a81db68abb8a1592f
Hille Brands <hilleannebrands@gmail.com>
2024-03-15 16:46:42 +0100

    "meaningful" comments

--- Scripts/table.js
+++ Scripts/table.js
@@ -1,5 +1,7 @@
 var tables = document.querySelectorAll("table");
 
+// For each table, add event listener to highlight
+// rows & cells when clicked on
 if (tables.length > 0) {
     for (var table of tables) {
         var rows = Array.from(table.rows);
