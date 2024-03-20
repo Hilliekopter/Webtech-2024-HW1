f88b0cb6139096ed2fcbc1ace933df998e4c8dea
Hille Brands <hilleannebrands@gmail.com>
2024-03-20 16:44:49 +0100

    public folder

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const path = require('path');
 
 const app = express();
 const port = 8022;
+app.use(express.static("public"));
 
 // sendFile will go here
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port);
