8845bcab92c8dc20eed2fe1529e9069119f61503
Hille Brands <hilleannebrands@gmail.com>
2024-03-22 13:38:14 +0100

    naar static verplaatst, login start

--- server.js
+++ server.js
@@ -10,5 +10,17 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/filming.html', function(req, res) {
+  res.sendFile(path.join(__dirname, 'filming.html'));
+});
+
+app.use(function(request, response) {
+  response.status(404).send("Page not found!");
+});
+
+// app.use() // Router gebruiken om tussen pagina's te navigeren
+
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
+
+// todo: login pagina -> sessions
